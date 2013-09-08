a0bf9944c9510a1fbc9f32a9c61b729cf2c26c22
xcorter <xcorter@mail.ru>
2013-09-09 02:27:07 +0700

    fix config

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 class Config {
     var $host = "localhost";
     var $db_name = "gallery";
-    var $db_user = "root";
-    var $db_pass = "zxcvbn";
+    var $db_user = "user1";
+    var $db_pass = "123456";
     var $url = "http://http://192.241.249.196/gallery/gallery/";
 }
 
