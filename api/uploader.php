8ac287ce13ea223779b91c7f7d29516837ba3573
xcorter <xcorter@mail.ru>
2013-09-09 09:37:50 +0700

    fix bug

--- api/uploader.php
+++ api/uploader.php
@@ -1,7 +1,6 @@
 <?php
 require_once("user.php");
 require_once("db.php");
-require_once("../config.php");
 
 class Uploader {
     var $allowedExts = array("gif", "jpeg", "jpg", "png");
@@ -17,28 +16,24 @@ class Uploader {
             echo "Please login";
             return false;
         }
-        echo 3;
-        $config = new Config();
         
         $arr = array();
         $temp = explode(".", $_FILES["image"]["name"]);
         $extension = end($temp);
-		echo 4;
         if (in_array($_FILES["image"]["type"], $this->allowedMimes)
         && ($_FILES["image"]["size"] < $this->size)) {
-			echo 5;
             if ($_FILES["image"]["error"] > 0) {
                 echo '[{"error":'. $_FILES["image"]["error"] .', "response": ""}]';
             } else {
                 $path = $this->folder.$user[1]."/".$_FILES["image"]["name"];
-                if (file_exists($config->path.$path)) {
+                if (file_exists($path)) {
                     echo '[{"error":"already exists", "response": ""}]';
                 } else {
-                    if (!file_exists($config->path.$this->folder.$user[1])) {
-                        mkdir($config->path.$this->folder.$user[1]);
+                    if (!file_exists($this->folder.$user[1])) {
+                        mkdir($this->folder.$user[1]);
                     }
                     $this->imageToDB($user[1], $path);
-                    move_uploaded_file($_FILES["image"]["tmp_name"], $config->path.$path);
+                    move_uploaded_file($_FILES["image"]["tmp_name"], $path);
                     echo '[{"error":"", "response": "'.$path.'"}]';
                 }
             }
